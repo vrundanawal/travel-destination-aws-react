8a228075dc3f38c32843e5f9892dbe25657c9428
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-08-05 11:10:14 +0100

    updated the package

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from "react";
+import { Loader, Placeholder } from "@aws-amplify/ui-react";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "@aws-amplify/ui-react/styles.css";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+Amplify.configure(outputs)
+
+const amplifyClient = generateClient<Schema>({
+    authMode: "userPool",
+});
+
+function App () {
+    const [result, setResult] = useState<string>("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setLoading(true);
+
+        try {
+            const formData = new FormData(event.currentTarget);
+            const { data, errors } = await amplifyClient.queries.askBedrock({
+                interests: [formData.get("interests")?.toString() || ""],
+            });
+
+            if (!errors) {
+                setResult(data?.body || "No data returned");
+            } else {
+                console.log(errors);
+            }
+
+            } catch (e) {
+                alert(`An error occurred: ${e}`);
+            } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div className="app-container">
+            <div className="header-container">
+                <h1 className="main-header">Meet Your Personal
+                    <span className="highlight"> Travel Destination Generator</span>
+                </h1>
+                <p className="description">
+                    Simply type a few interests using the format interest1, interest2, etc...
+                    and the generator will comeback with great destinations for you.
+                </p>
+            </div>
+
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="search-container">
+                    <input
+                        type="text"
+                        className="wide-input"
+                        id="interests"
+                        name="interests"
+                        placeholder="interest1, interest2, interest3,...etc"
+                    />
+                    <button type="submit" className="search-button">
+                        Generate
+                    </button>
+                </div>
+            </form>
+
+            <div className="result-container">
+                {loading ? (
+                    <div className="loader-container">
+                        <p>Loading...</p>
+                        <Loader size="large"/>
+                        <Placeholder size="large"/>
+                        <Placeholder size="large"/>
+                        <Placeholder size="large"/>
+                    </div>
+                ) : (
+                    result && <p className="result">{result}</p>
+                )}
+            </div>
+        </div>
+    )
 }
 
-export default App
+export default App;
